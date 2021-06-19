0f9b001a5da8f1fa25a7220a13693d14a03667fa
Benjamin <milk500ted939@gmail.com>
2021-06-19 13:06:12 +0800

    modify Z aca

--- other/DS/Binary_Trie.cpp
+++ other/DS/Binary_Trie.cpp
@@ -1,20 +1,58 @@
+const int LOG = 30;
 struct Node {
-	int cnt;
+	int sz;
 	Node *ch[2];
 	Node() {
-		cnt = 0;
-		ch[0] = ch[1] = NULL;
+		sz = 0;
+		ch[0] = ch[1] = nullptr;
+	}
+	static get_size(Node *d) {
+		return d == nullptr ? 0 : d->sz;
+	}
+	void insert(int x) {
+		Node *cur = *this;
+		for (int i = LOG - 1; i >= 0; i--) {
+			int to = x >> i & 1;
+			cur->sz++;
+			if (root->ch[to] == nullptr) {
+				cur->ch[to] = new Node();
+			}
+			cur = cur->ch[to];
+		}
+		root->sz++;
+	}
+	void find(int x) {
+		Node *cur = *this;
+		for (int i = LOG - 1; i >= 0; i--) {
+			int to = x >> i & 1;
+			if (root->ch[to] == nullptr) {
+				return 0;
+			}
+			cur = cur->ch[to];
+		}
+		return 1;
+	}
+	int erase(int x) {
+		if (!find(x)) return 0;
+		Node *cur = *this;
+		for (int i = LOG - 1; i >= 0; i--) {
+			int to = x >> i & 1;
+			cur->sz--;
+			cur = cur->ch[to];
+		}
+		root->sz--;
+		return 1;
+	}
+	int max_xor(int x) {
+		// maximum value of x ^ (some value in trie)
+		Node *cur = *this;
+		for (int i = LOG - 1; i >= 0; i--) {
+			int to = (x >> i & 1) ^ 1;
+			if (get_size(cur->ch[to]) == 0) {
+				to ^= 1;
+			}
+			x ^= to << i;
+		}
+		return x;
 	}
 };
- 
-void BTinsert(Node *root, int x) {
-	for (int i = BIT; i >= 0; i--) {
-		int to = (x >> i & 1);
-		root->cnt++;
-		if (!root->ch[to]) {
-			root->ch[to] = new Node();
-		}
-		root = root->ch[to];
-	}
-	root->cnt++;
-}
