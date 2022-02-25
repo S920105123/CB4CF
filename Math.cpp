f08dc47f43ca682a9b99ae3735c2fb25b09fc0b3
Benjamin <milk500ted939@gmail.com>
2022-02-25 23:29:46 +0800

    Add Stirling

--- Math.cpp
+++ Math.cpp
@@ -16,22 +16,19 @@ LL mod_inv(LL a, LL mod) {
  
 /* fac, C, H */
 const int LIM = 1000000; // Double for H(n, k)
-const LL M_MOD = MOD;
 LL fac[LIM], finv[LIM], inv[LIM];
 void init_fac() {
 	fac[0] = finv[0] = 1;
 	for (int i = 1; i < LIM; i++) {
-		fac[i] = fac[i - 1] * i % M_MOD;
-		inv[i] = i == 1 ? 1 : M_MOD - M_MOD / i * inv[M_MOD % i] % M_MOD;
-	}
-	for (int i = 1; i < LIM; i++) {
-		finv[i] = finv[i - 1] * inv[i] % M_MOD;
+		fac[i] = fac[i - 1] * i % MOD;
+		inv[i] = i == 1 ? 1 : MOD - MOD / i * inv[MOD % i] % MOD;
+		finv[i] = finv[i - 1] * inv[i] % MOD;
 	}
 }
 LL C(LL n, LL k) {
-	assert(n < M_MOD && k < M_MOD); // otherwise, use Lucas
+	assert(n < MOD && k < MOD); // otherwise, use Lucas
 	if (k > n || n < 0 || k < 0) return 0;
-	return fac[n] * finv[k] % M_MOD * finv[n - k] % M_MOD;
+	return fac[n] * finv[k] % MOD * finv[n - k] % MOD;
 }
 LL H(LL n, LL k) {
 	// n boxes, k items.
@@ -40,8 +37,9 @@ LL H(LL n, LL k) {
 // C(n, k) % M_MOD, M_MOD is a prime, requie fac[1 ... M_MOD-1]
 LL Lucas(LL n, LL k) {
 	LL ans = 1;
-	for (; n > 0 || k > 0; n /= M_MOD, k /= M_MOD) {
-		ans = ans * C(n % M_MOD, k % M_MOD) % M_MOD;
+	for (; n > 0 || k > 0; n /= MOD, k /= MOD) {
+		ans = ans * C(n % MOD, k % MOD) % MOD;
 	}
 	return ans;
 }
+
--- other/Math/NTT.cpp
+++ other/Math/NTT.cpp
@@ -1,18 +1,36 @@
-const LL mod = 998244353;
-const LL p_root = 3;
-const LL root_pw = 1LL << 23;
-
-// Do NTT under mod. invert=true to do iNTT.
-// mod MUST be a prime, if mod=c*2^k+1, then
+// Do NTT under N_MOD. invert=true to do iNTT.
+// mod MUST be a prime, if mod = c*2^k+1, then
 // p_root is any primitive root of mod
 // root_pw=2^k, and n(size) MUST <= 2^k
 // n MUST be power of 2.
 // mod=2013265921, root_pw=1LL<<27, p_root=31
 
-void ntt(LL a[], int n, bool invert) {
-    LL root = qpow(p_root, (mod-1)/root_pw, mod);
-	LL root_1 = mod_inv(root, mod);
+// NOTE: all input numbers should be in [0, MOD), n must be power of 2
+//const LL MOD = 998244353; // here
+const LL p_root = 3;
+const LL root_pw = 1LL << 23;
 
+LL qpow(LL a, LL pw) {
+	LL res = 1;
+	a %= MOD;
+	while (pw) {
+		if (pw & 1) res = res * a % MOD;
+		a = a * a % MOD;
+		pw >>= 1;
+	}
+	return res;
+}
+
+LL mod_inv(LL a) {
+	// for prime
+	a %= MOD;
+	return qpow(a, MOD - 2);
+}
+
+void ntt(vector<LL> &A, bool invert) {
+	int n = A.size(); 
+    LL root = qpow(p_root, (MOD-1)/root_pw);
+	LL root_1 = mod_inv(root);
 	for (int i = 1, j = 0; i < n; i++) {
 		LL bit = n >> 1;
 		for (; j & bit; bit >>= 1)
@@ -20,29 +38,29 @@ void ntt(LL a[], int n, bool invert) {
 		j ^= bit;
 
 		if (i < j)
-			swap(a[i], a[j]);
+			swap(A[i], A[j]);
 	}
 
 	for (int len = 2; len <= n; len <<= 1) {
 		LL wlen = invert ? root_1 : root;
 		for (int i = len; i < root_pw; i <<= 1)
-			wlen = wlen * wlen % mod;
+			wlen = wlen * wlen % MOD;
 
 		for (int i = 0; i < n; i += len) {
 			LL w = 1;
 			for (int j = 0; j < len / 2; j++) {
-				LL u = a[i+j], v = a[i+j+len/2] * w % mod;
-				a[i+j] = u + v < mod ? u + v : u + v - mod;
-				a[i+j+len/2] = u - v >= 0 ? u - v : u - v + mod;
-				w = w * wlen % mod;
+				LL u = A[i+j], v = A[i+j+len/2] * w % MOD;
+				A[i+j] = u + v < MOD ? u + v : u + v - MOD;
+				A[i+j+len/2] = u - v >= 0 ? u - v : u - v + MOD;
+				w = w * wlen % MOD;
 			}
 		}
 	}
 
 	if (invert) {
-		LL n_1 = mod_inv(n, mod);
+		LL n_1 = mod_inv(n);
 		for (int i = 0; i < n; i++) {
-			a[i] = a[i] * n_1 % mod;
+			A[i] = A[i] * n_1 % MOD;
 		}
 	}
 }
