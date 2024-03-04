0ad20a8536320a4f81aad6efff5715344a06733f
Wen-Horng Sheu <wsheu@ucdavis.edu>
2024-03-03 16:24:44 -0800

    update new Lagrange interpolation

--- library/Math/Lagrange.cpp
+++ library/Math/Lagrange.cpp
@@ -1,29 +1,59 @@
-LL find_nth_term(vector<LL> ys, LL n, LL mod) {
-	// There's an unknwon polynomial P of degree at most k such that (P(0) ... P(k)) are ys[0 ... k+1]
-	// Return P(n) % mod in O(k lg k), mod must be prime
-	int k = (int)ys.size() - 1;
-	n %= mod;
-	for (LL &y : ys) {
-		y = (y % mod + mod) % mod;
-	}
-	if (n <= k) {
-		return ys[n];
-	}
-	
-	LL prod = 1, deno = 1;
-	for (LL i = 1; i <= k; i++) {
-		deno = deno * -i % mod;
-		prod = prod * (n - i) % mod;
-	}
-	prod = prod * mod_inv((deno + mod) % mod, mod) % mod;
-	
-	LL res = 0;
-	for (LL i = 0; i <= k; i++) {
-		res = (res + prod * ys[i]) % mod;
-		if (i < k) {
-			prod = prod * mod_inv((i + 1) * (n - i - 1) % mod, mod) % mod;
-			prod = prod * (i - k + mod) % mod * (n - i) % mod;
+struct Lagrange {
+	// Implement lagrange interpolation
+	int maxd;
+	vector<LL> fact, finv, inv;
+//	const LL MOD = (LL) 1e9 + 7; // MOD must be a prime
+	void init(int max_deg) {
+		// max_deg is the maximum degree of input polynomial
+		// Complexity = O(max_deg)
+		maxd = max_deg;
+		fact = vector<LL>(maxd + 1, 1);
+		finv = vector<LL>(maxd + 1, 1);
+		inv = vector<LL>(maxd + 1, 1);
+		for (int i = 1; i <= maxd; i++) {
+			fact[i] = fact[i - 1] * i % MOD;
+			inv[i] = i == 1 ? 1 : MOD - MOD / i * inv[MOD % i] % MOD;
+			finv[i] = finv[i - 1] * inv[i] % MOD;
 		}
 	}
-	return res;
-}
+	LL interpolate(vector<LL> ys, LL t) {
+		// There's a polynomial P(x) of degree d = ys.size() - 1. (The leading term is x^d)
+		// We know (P(0) ... P(d)), which are stored in ys[0 ... d]
+		// Evaluate P(t) % MOD
+		// Complexity is O(d) = O(ys.size())
+		assert(ys.size() <= maxd + 1); // This line says that you cannot interpolate a polynomial of degree > maxd
+		int d = (int)ys.size() - 1;
+		t %= MOD;
+		for (LL &y : ys) {
+			y = (y % MOD + MOD) % MOD;
+		}
+		if (t <= d) {
+			return ys[t];
+		}
+		
+		vector<LL> pref(d + 1), suff(d + 1);
+		pref[0] = t;
+		for (int i = 1; i <= d; i++) {
+			pref[i] = pref[i - 1] * (t - i + MOD) % MOD;
+		}
+		suff[d] = (t - d + MOD) % MOD;
+		for (int i = d - 1; i >= 0; i--) {
+			suff[i] = suff[i + 1] * (t - i + MOD) % MOD;
+		}
+		
+		LL result = 0;
+		for (int i = 0; i <= d; i++) {
+			LL term = 1;
+			if (i > 0) {
+				term = term * pref[i - 1] % MOD;
+				term = term * finv[i] % MOD;
+			}
+			if (i < d) {
+				term = term * suff[i + 1] % MOD;
+				term = term * finv[d - i] * ((d - i) % 2 ? -1 : 1) % MOD;
+			}
+			result = (result + ys[i] * term % MOD + MOD) % MOD;
+		}
+		return result;
+	}
+} algo;
